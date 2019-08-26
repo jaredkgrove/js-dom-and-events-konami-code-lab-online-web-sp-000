b17ab25a9dc3ad70dbcca0ddb9abaa763dd154d7
Jared Grove <jaredkgrove@gmail.com>
2019-08-26 15:12:21 -0500

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,18 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-
+  let konamiIndex = 0
+  const konamiFunction = e => {
+    
+    if( parseInt(e.detail || e.which) === code[konamiIndex]){
+      konamiIndex++
+      if(konamiIndex === code.length){
+        alert("Good on you")
+        konamiIndex = 0
+      }
+    }else{
+      konamiIndex = 0
+    }
+  }
+  document.body.addEventListener('keydown', konamiFunction)
 }
